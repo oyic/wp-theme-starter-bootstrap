ad848169f7f3b25950d56fd1781939d3a61dc595
= <=>
2018-10-03 03:36:39 +0800

    Include Parallax on the build

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 
 	<?php wp_head(); ?>		
 	</head>
-	<body <?php body_class(); ?>>
+	<body <?php body_class('container'); ?>>
 	<div class="main-wrapper">
 		<header class="header">
 			<nav class="navbar navbar-expand-md navbar-light bg-faded">
@@ -24,6 +24,19 @@
 			     'container_class' => 'collapse navbar-collapse',
 			     'menu_id'         => false,
 			     'menu_class'      => 'navbar-nav mr-auto',
+			     'depth'           => 2,
+			     'fallback_cb'     => 'bs4navwalker::fallback',
+			     'walker'          => new bs4navwalker()
+			   ]);
+			   ?>
+			   <?php
+			   wp_nav_menu([
+			     'theme_location'  => 'primary',
+			     'container'       => 'div',
+			     'container_id'    => 'bs4navbar',
+			     'container_class' => 'collapse navbar-collapse',
+			     'menu_id'         => false,
+			     'menu_class'      => 'navbar-nav mr-auto ml-auto',
 			     'depth'           => 2,
 			     'fallback_cb'     => 'bs4navwalker::fallback',
 			     'walker'          => new bs4navwalker()
--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@
     
     import map from "./map";
     import 'bootstrap';
+    import parallax from 'parallax-scroll';
     $(document).ready(function(){
     	console.log('hello there?');
     	
@@ -17,5 +18,18 @@
 	    //     // live_search action is define in WP with add_action('wp_ajax_nopriv_live_search','live_search')
 	    //     data:{action:"live_search",key:$(this).val()}, 
 	    // })
+	    // 
+	    // 
+	    // PARALLAX
+	    // =========================
+	    //const parallax = new Parallax('.js-parallax', {
+		//   speed: 0.2, // Anything over 0.5 looks silly
+		// });
+		// parallax.animate();
+		// ======================
+		// html sample
+		// <div class="o-banner">
+		//   <div class="o-banner__img js-parallax" style="background-image: url(path/to/some/img.jpg);"></div>
+		// </div>
 
     })
